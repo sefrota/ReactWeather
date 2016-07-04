e228cdb1dfb7c8ca7aff0cddc106d796adb878c4
sefrota <miguel265>
2016-07-04 22:13:13 +0100

    styled about page

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 /*var About = React.createClass({
 	render: function(){
 		return (
@@ -12,8 +12,20 @@ var React = require('react');
 var About = (props) => {
 	return (
 	<div>
-		<h3>About</h3>
-		<p>Welcome to the about Component</p>
+		<h1 className ='text-center'>About</h1>
+		<p>This app was created to provide people the ability to know about the weather anywhere in the world.</p>
+		<p>For more info visit the following links:</p>
+		<ol>
+			<li>
+				<a href="https://github.com/sefrota/ReactWeather">ReactWeather@github</a>
+			</li>
+			<li>
+				<a href="http://foundation.zurb.com/">@Foundation</a>
+			</li>
+			<li>
+				<a href="https://facebook.github.io/react/">@React</a>
+			</li>
+		</ol>
 	</div>
 	);
 }
